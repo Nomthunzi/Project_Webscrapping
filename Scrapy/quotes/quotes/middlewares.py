ede1ae31ba6c693e965322ca1873c1cf1dabb3f9
Nomthunzi <75237787+Nomthunzi@users.noreply.github.com>
2021-05-09 23:33:01 +0200

    Update middlewares.py

--- Scrapy/quotes/quotes/middlewares.py
+++ Scrapy/quotes/quotes/middlewares.py
@@ -4,9 +4,7 @@ from itemadapter import is_item, ItemAdapter
 
 
 class QuotesSpiderMiddleware:
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the spider middleware does not modify the
-    # passed objects.
+    
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -16,33 +14,21 @@ class QuotesSpiderMiddleware:
         return s
 
     def process_spider_input(self, response, spider):
-        # Called for each response that goes through the spider
-        # middleware and into the spider.
 
-        # Should return None or raise an exception.
         return None
 
     def process_spider_output(self, response, result, spider):
-        # Called with the results returned from the Spider, after
-        # it has processed the response.
-
+        
         # Must return an iterable of Request, or item objects.
         for i in result:
             yield i
 
     def process_spider_exception(self, response, exception, spider):
-        # Called when a spider or process_spider_input() method
-        # (from other spider middleware) raises an exception.
-
-        # Should return either None or an iterable of Request or item objects.
+        
         pass
 
     def process_start_requests(self, start_requests, spider):
-        # Called with the start requests of the spider, and works
-        # similarly to the process_spider_output() method, except
-        # that it doesn’t have a response associated.
-
-        # Must return only requests (not items).
+       
         for r in start_requests:
             yield r
 
@@ -51,9 +37,7 @@ class QuotesSpiderMiddleware:
 
 
 class QuotesDownloaderMiddleware:
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the downloader middleware does not modify the
-    # passed objects.
+    
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -63,19 +47,13 @@ class QuotesDownloaderMiddleware:
         return s
 
     def process_request(self, request, spider):
-        # Called for each request that goes through the downloader
-        # middleware.
+        
 
         
         return None
 
     def process_response(self, request, response, spider):
-        # Called with the response returned from the downloader.
-
-        # Must either;
-        # - return a Response object
-        # - return a Request object
-        # - or raise IgnoreRequest
+       
         return response
 
     def process_exception(self, request, exception, spider):
