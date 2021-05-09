9ae4a815171e1494350a45cc4bc35edfb5046d14
Nomthunzi <75237787+Nomthunzi@users.noreply.github.com>
2021-05-09 23:29:09 +0200

    Update middlewares.py

--- Scrapy/quotes/quotes/middlewares.py
+++ Scrapy/quotes/quotes/middlewares.py
@@ -1,11 +1,5 @@
-# Define here the models for your spider middleware
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-
 from scrapy import signals
 
-# useful for handling different item types with a single interface
 from itemadapter import is_item, ItemAdapter
 
 
@@ -72,12 +66,7 @@ class QuotesDownloaderMiddleware:
         # Called for each request that goes through the downloader
         # middleware.
 
-        # Must either:
-        # - return None: continue processing this request
-        # - or return a Response object
-        # - or return a Request object
-        # - or raise IgnoreRequest: process_exception() methods of
-        #   installed downloader middleware will be called
+        
         return None
 
     def process_response(self, request, response, spider):
@@ -90,13 +79,7 @@ class QuotesDownloaderMiddleware:
         return response
 
     def process_exception(self, request, exception, spider):
-        # Called when a download handler or a process_request()
-        # (from other downloader middleware) raises an exception.
-
-        # Must either:
-        # - return None: continue processing this exception
-        # - return a Response object: stops process_exception() chain
-        # - return a Request object: stops process_exception() chain
+        
         pass
 
     def spider_opened(self, spider):
